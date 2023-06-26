41fdd6f311e5e7afd1b2591cbe6c7bf8cb9e6ada
Lucianosolari <luciano3924@gmail.com>
2023-06-25 21:13:15 -0300

    disco c

--- BackendBolsaDeTrabajoUTN/Program.cs
+++ BackendBolsaDeTrabajoUTN/Program.cs
@@ -47,6 +47,7 @@ builder.Services.AddDbContext<TPContext>(dbContextOptions =>
         new MySqlServerVersion(new Version(8, 0, 22)))
 );
 
+
 builder.Services.AddAuthentication("Bearer") //"Bearer" es el tipo de auntenticación que tenemos que elegir después en PostMan para pasarle el token
     .AddJwtBearer(options => //Acá definimos la configuración de la autenticación. le decimos qué cosas queremos comprobar. La fecha de expiración se valida por defecto.
     {
